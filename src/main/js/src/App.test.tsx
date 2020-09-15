03386ace8ebfde3c3c8c46e4e4cee124759946e4
Kathan Cheema <kathancheema@outlook.com>
2020-09-15 21:08:09 +0100

    Fix test

--- src/main/js/src/App.test.tsx
+++ src/main/js/src/App.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {render, waitForElement} from '@testing-library/react';
-import {getByText} from "@testing-library/react";
 import App from './App';
 
 test('renders app', async () => {
@@ -8,5 +7,5 @@ test('renders app', async () => {
 
   const text = await waitForElement(() => getAllByText(/Invoice/));
 
-  expect(text).toBeInTheDocument();
+  text.forEach(t => expect(t).toBeInTheDocument());
 });
